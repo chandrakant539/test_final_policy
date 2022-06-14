5efb151972a84a40fd480c4a4ed4b4502d3e0625
chandrakant539 <62873644+chandrakant539@users.noreply.github.com>
2022-06-14 16:27:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 # Configure the AWS Provider
 provider "aws" {
-  region     = "us-west-2"
+  region = "us-west-2"
+  
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
   
 }
 
